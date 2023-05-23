6e9c61d657a59c39a99210e2de239347d79b27a9
Andrew <as-pasa@mail.ru>
2023-05-23 18:33:17 +0500

    #3 change of TargetRepresentation to term model

--- robots/src/gui/drawModels/TargetRepresentation.java
+++ robots/src/gui/drawModels/TargetRepresentation.java
@@ -7,35 +7,27 @@ import models.states.TargetStateReader;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 
-import static utils.DrawUtils.*;
 import static utils.MathUtils.*;
 
-public class TargetRepresentation  {
+public class TargetRepresentation {
 
 
-    private Color m_bodyColor;
-    private Color m_edgeColor;
-
+    private RepresentationTermOval m_body;
 
     public TargetRepresentation() {
-        m_bodyColor=Color.GREEN;
-        m_edgeColor=Color.BLACK;
+        m_body = new RepresentationTermOval("body", 5, 5, Color.GREEN);
     }
 
-    public TargetRepresentation(Color m_bodyColor, Color m_edgeColor) {
-        this.m_bodyColor = m_bodyColor;
-        this.m_edgeColor = m_edgeColor;
+    public TargetRepresentation(Color m_bodyColor) {
+        m_body = new RepresentationTermOval("body", 5, 5, m_bodyColor);
     }
 
 
-    public void draw(Graphics2D g,TargetModel model) {
-        TargetStateReader targetCoord=model.getState();
+    public void draw(Graphics2D g, TargetModel model) {
+        TargetStateReader targetCoord = model.getState();
         AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0);
         g.setTransform(t);
-        g.setColor(m_bodyColor);
-        fillOval(g, round(targetCoord.getX()) ,round(targetCoord.getY()) , 5, 5);
-        g.setColor(m_edgeColor);
-        drawOval(g, round(targetCoord.getX()) ,round(targetCoord.getY()),  5, 5);
+        m_body.draw(g, round(targetCoord.getX()), round(targetCoord.getY()));
     }
 
 }
